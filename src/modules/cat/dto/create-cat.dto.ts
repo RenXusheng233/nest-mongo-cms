3517ff9634bdebeeaf2f04a6b4c8a5d8c703bae4
RenXusheng233 <782449909@qq.com>
2024-02-29 18:56:49 +0800

    feat: class-validator

--- src/modules/cat/dto/create-cat.dto.ts
+++ src/modules/cat/dto/create-cat.dto.ts
@@ -1,1 +1,16 @@
-export class CreateCatDto {}
+import { IsInt, IsString, Length, Max, Min } from 'class-validator';
+
+export class CreateCatDto {
+  @Length(1, 99)
+  @IsString()
+  name: string;
+
+  @IsInt()
+  @Min(0)
+  @Max(99)
+  age: number;
+
+  @IsString()
+  @Length(1, 99)
+  breed: string;
+}
